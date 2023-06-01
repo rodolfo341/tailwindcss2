5304c8751192145952bea8bfb4211b4378e1c33c
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-05-31 21:29:04 -0400

    tailwind responsivo

--- paso-a-paso/tailwind.config.js
+++ paso-a-paso/tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+
+	content: ["./src/**/*.{html,js}"],
+
+	theme: {
+
+		container: {
+			center: true,
+		},
+
+		extend: {
+			colors: {
+				"peru-1": "#cd853f",
+				"peru-2": "#f8bb7e",
+				"peru-3": "#f8cda1",
+			},
+		},
+
+	},
+	plugins: [],
 }

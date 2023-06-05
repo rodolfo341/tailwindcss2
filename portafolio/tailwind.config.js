9ea9c3d2458ff6e39c1b79cbaec24fc7955038a9
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-06-04 22:00:20 -0400

    tailwind fuente

--- portafolio/tailwind.config.js
+++ portafolio/tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
         primary: "#0f172a",
-      }
+      },
+      fontFamily:{
+        roboto:["Roboto", ...defaultTheme.fontFamily.sans],
+      },
     },
   },
   plugins: [],

1d7660a3f89c2d79ca9e09e33d864bfae400d58c
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-06-04 21:32:11 -0400

    tailwind color de fondo en claro y dark

--- portafolio/tailwind.config.js
+++ portafolio/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0f172a",
+      }
+    },
   },
   plugins: [],
 }
